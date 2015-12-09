38c728b7787ddcc9b1fec9ad70f25b3d0733e752
EvanSchallerer <schallee@oregonstate.edu>
2015-12-08 22:15:10 -0800

    Added test for row, column and box elimination of values

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
+import java.io.Console;
 
 public class Main {
 	
 	public static void main(String [] args){
 		SudokuBoard board = new SudokuBoard(args[0]);
 		board.print();
+		testEliminateValuesStrategy(board);
+	}
+	
+	public static void testEliminateValuesStrategy(SudokuBoard board){
+		IStrategy s = new EliminateByRowColumnBoxStrategy();
+		board = s.attempt(board);
+		assert !board.board[1][8].possibleValues.contains(1);
+		assert !board.board[1][8].possibleValues.contains(5);
+		assert !board.board[1][8].possibleValues.contains(6);
+
+		System.out.println("TEST PASSED!");
 	}
 
 }
