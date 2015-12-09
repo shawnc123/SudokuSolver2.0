569c40779fae36fe8a87a7d127771f157ed08331
EvanSchallerer <schallee@oregonstate.edu>
2015-12-08 22:35:07 -0800

    More testing

--- src/Main.java
+++ src/Main.java
@@ -11,9 +11,22 @@ public class Main {
 	public static void testEliminateValuesStrategy(SudokuBoard board){
 		IStrategy s = new EliminateByRowColumnBoxStrategy();
 		board = s.attempt(board);
+		assert board.board[1][8].getValue() == null;
 		assert !board.board[1][8].possibleValues.contains(1);
 		assert !board.board[1][8].possibleValues.contains(5);
 		assert !board.board[1][8].possibleValues.contains(6);
+		
+		assert board.board[8][8].getValue() == null;
+		assert !board.board[8][8].possibleValues.contains(5);
+		assert !board.board[8][8].possibleValues.contains(6);
+		assert !board.board[8][8].possibleValues.contains(1);
+		assert !board.board[8][8].possibleValues.contains(9);
+		assert !board.board[8][8].possibleValues.contains(8);
+		assert !board.board[8][8].possibleValues.contains(3);
+		assert !board.board[8][8].possibleValues.contains(2);
+		assert board.board[8][8].possibleValues.size() == 2;
+		
+		assert board.board[7][3].getValue() == 8;
 
 		System.out.println("TEST PASSED!");
 	}
